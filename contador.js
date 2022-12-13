78a902093d52dec290e759237fe62ab48bb5e501
guidodenigris <abuela_00@hotmail.com>
2022-12-13 19:38:42 -0300

    primer desafio

---
+++ contador.js
@@ -0,0 +1,32 @@
+class ProductManager {
+    products;
+    constructor() {
+      this.products = []
+    }
+    getProducts () {
+        return this.products;
+    }
+    addProduct (
+        title,
+        id,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+    )
+    
+    if (productsid === productsId){
+        products.cantidad++
+
+    }else{
+        const item = productos.find((products) => products.id === productsId)
+
+        products.push(item)
+    }
+    
+}
+
+const agregarProductos = new AddProduct ()
+agregarProductos.AddProduct ('Primer desafio', 1, 'Este es el primer desafio', 1500, 'noimage', 'code123', 25)
+
